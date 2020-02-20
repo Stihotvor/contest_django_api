f180d5cb346d6e60f2e1fcd68a3a3cf7709b3cac
Phoenix-IT <dyachenko.yevhen@gmail.com>
2020-02-20 14:01:36 +0100

    Project setup command

---
+++ netcovapp/management/commands/project_setup.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from netcovapp.models import Operator
+from netcovapp.services.coverage_tools import update_coverage
+
+
+class Command(BaseCommand):
+    help = "Operators setup"
+
+    def handle(self, *args, **options):
+        try:
+            operators = [
+                Operator(name='Orange', code=20801),
+                Operator(name='SFR', code=20810),
+                Operator(name='Free', code=20815),
+                Operator(name='Bouygue', code=20820),
+                ]
+            Operator.Objects.bulk_create(operators)
+            self.stdout.write(self.style.SUCCESS("Project ready"))
+        except Exception as e:
+            raise CommandError(e)
